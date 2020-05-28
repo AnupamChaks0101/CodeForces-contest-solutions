8bb7c7ce0271434ff751400fe5f85dc83efd8e03
Anupam Chakraborty <53541650+AnupamChaks0101@users.noreply.github.com>
2020-05-29 02:50:12 +0530

    Create EcFc_87_A.py

---
+++ Educational Codeforces Round 87(Div. 2)/EcFc_87_A.py
@@ -0,0 +1,25 @@
+#Question --> A. Alarm Clock
+
+import math
+t = int(input())
+while(t>0):
+    a = list(map(float,input().split()))
+    if(a[1]>=a[0]):
+        print(int(a[1]))
+    elif(a[2]-a[3]<=0):
+        print("-1")    
+    else:
+        df = a[2]-a[3]
+        if(df==1):
+            a[0] = int(a[0])
+            a[1] = int(a[1])
+            a[2] = int(a[2])
+            a[3] = int(a[3])
+            ts = ((a[0]-a[1])*a[2])
+            ts = int(ts + a[1])
+            print(int(ts))
+        else:
+            a[0] = a[0]-a[1]
+            ts = (math.ceil(a[0]/df))*a[2]+a[1]
+            print(int(ts))
+    t =t-1
